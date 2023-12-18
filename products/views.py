ad1ee29fb97fcaa529b5fc468255231c0bce948f
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2023-12-18 23:08:23 +0300

    Added models of product cotegory and products themselves
    Added models of product category and products themselves,  and also made the first migrations

--- products/views.py
+++ products/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {"title": "Store",
-            }
+    context = {"title": "Store",}
     return render(request, "products/index.html", context=context)
 
 
