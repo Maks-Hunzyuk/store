258e12fb156d0386c1e673940ed2cbd724952d3f
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2023-12-18 19:46:55 +0300

    Created the products application

--- store/settings.py
+++ store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q7%u5(+*2(3h52#9@+h9*_i4!9*+r74ee05wiipra6g)-2h2@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'products',
 ]
 
 MIDDLEWARE = [
