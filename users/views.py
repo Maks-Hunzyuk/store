cb9f860c340b22b30cf92bccb3731459fc3dc053
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2023-12-19 22:56:38 +0300

    19.12 Added authorization
    Added controller, form, authorizations

--- users/views.py
+++ users/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user=user)
+                return HttpResponseRedirect(reverse("index"))
+    else:
+        form = UserLoginForm()
     context = {'form': UserLoginForm()}
     return render(request, "users/login.html", context=context)
 
 
 def registration(request):
-    return render(request, "users/registration.html")
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegistrationForm()
+    context = {"form": form}
+    return render(request, "users/registration.html", context)
