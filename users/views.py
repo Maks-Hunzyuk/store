91e1605a2424520fccb38e31829f3be8fef9795a
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2023-12-20 21:13:55 +0300

    20.12 Added a cart of goods
    added logic for working with the goods cart

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
 
 
 def login(request):
@@ -35,6 +37,7 @@ def registration(request):
     return render(request, template_name="users/registration.html", context=context)
 
 
+@login_required
 def profile(request):
     if request.method == "POST":
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -43,7 +46,11 @@ def profile(request):
             return HttpResponseRedirect(reverse("users:profile"))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {"title": "Store - Профиль", "form": form}
+
+    context = {"title": "Store - Профиль",
+               "form": form,
+               "baskets": Basket.objects.filter(user=request.user)
+             }
     return render(request, template_name="users/profile.html", context=context)
 
 
