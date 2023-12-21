4fd53619d6bd47a5c8bc8d950d2b768a7683a002
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2023-12-21 19:11:44 +0300

    21/12 Added pogination and filtering by category
    Added pogination and filtering by category

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from products.models import Product, ProductCategory, Basket
 from users.models import User
@@ -10,10 +11,18 @@ def index(request):
     return render(request, template_name="products/index.html", context=context)
 
 
-def products(request):
+def products(request, category_id=None, page_number=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
     context = {
         'title': "Store - Каталог",
-        'products': Product.objects.all(),
+        'products': products_paginator,
         'categories': ProductCategory.objects.all()
     }
     return render(request, template_name="products/products.html", context=context)
